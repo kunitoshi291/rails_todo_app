80f46aec72e4f8f3b3855a040dcd5f550023da99
kunitoshi291 <takevadogu@outlook.jp>
2022-06-28 13:44:13 +0000

    初期コミット_チェックボックスのトグル動作の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,40 @@ class TasksController < ApplicationController
       render 'new', status: :unprocessable_entity
     end
   end
+  # todo新規作成
+  def edit 
+    # p params[:id]
+    @task = Task.find(params[:id])
+  end
+  # 更新処理
+  def update
+      @task = Task.find(params[:id])
+      if @task.update(task_params)
+        redirect_to root_path
+      else
+        render 'edit', status: :unprocessable_entity 
+      end
+  end
+  # 削除処理
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path, status: :see_other
+  end
+  
+  def toggle
+    @task = Task.find(params[:id])
+    # 現在のデータベースに保存されているcompletedの値を反転してデータベースの値を更新
+    @task.update(completed: !@task.completed)
+    render turbo_stream: turbo_stream.replace(
+      # taskオブジェクト自動生成するDOMID
+      @task,
+      # 置き換えに指定するpartialを指定
+      partial: 'completed',
+      # partialに渡すパラメータを指定
+      locals: {task: @task}
+      ) 
+  end
   
   # これより以下は、プライベートメソッドとして扱う
   private
@@ -24,3 +58,4 @@ class TasksController < ApplicationController
     params.require(:task).permit(:title)
   end
 end
+
